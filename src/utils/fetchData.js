ce2421133f090b464fe73e830ba38a335b3108db
Oleg Skakunov <dosmds@gmail.com>
2025-04-13 10:57:03 +0300

    перевел на работу через env файл

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,10 @@
+import { API_URL } from "../components/config/env";
+
 export const fetchData = async () => {
 	const result = { data: null, error: null };
 
 	try {
-		const response = await fetch(
-			"https://jsonplaceholder.typicode.com/todos"
-		);
+		const response = await fetch(`${API_URL}/todos`);
 		const data = await response.json();
 		result.data = data;
 	} catch (error) {
